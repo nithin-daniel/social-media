2cb4b97dc6c7f444c09b19c566413835d56efde5
nithindaniel1 <nithindaniel2018@gmail.com>
2024-01-31 14:40:14 +0530

    added models to admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post,Like,DisLike
 # Register your models here.
+admin.site.register(Like)
+admin.site.register(Post)
+admin.site.register(DisLike)
